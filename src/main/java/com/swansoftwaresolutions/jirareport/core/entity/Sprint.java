28815c27746437edfcd21b7e7b3ae6b4c1f7eb68
waldemar <zehelloworld@gmail.com>
2016-02-22 14:46:22 +0200

    Add sprint entity

---
+++ src/main/java/com/swansoftwaresolutions/jirareport/core/entity/Sprint.java
@@ -0,0 +1,137 @@
+package com.swansoftwaresolutions.jirareport.core.entity;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * @author Vladimir Martynyuk
+ */
+
+@Entity
+@Table(name = "sprints")
+public class Sprint implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "report_id")
+    private Long reportId;
+
+    @Column(name = "agile_sprint_id")
+    private Long agileSprintId;
+
+    @Column(name = "not_count_target")
+    private Long notCountTarget;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "state")
+    private String state;
+
+    @Column(name = "type")
+    private Long type;
+
+    @Column(name = "start_date")
+    private Date startDate;
+
+    @Column(name = "end_date")
+    private Date endDate;
+
+    @Column(name = "complete_date")
+    private Date completeDate;
+
+    @Column(name = "show_uat")
+    private Long showUat;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getReportId() {
+        return reportId;
+    }
+
+    public void setReportId(Long reportId) {
+        this.reportId = reportId;
+    }
+
+    public Long getAgileSprintId() {
+        return agileSprintId;
+    }
+
+    public void setAgileSprintId(Long agileSprintId) {
+        this.agileSprintId = agileSprintId;
+    }
+
+    public Long getNotCountTarget() {
+        return notCountTarget;
+    }
+
+    public void setNotCountTarget(Long notCountTarget) {
+        this.notCountTarget = notCountTarget;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public Long getType() {
+        return type;
+    }
+
+    public void setType(Long type) {
+        this.type = type;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public Date getCompleteDate() {
+        return completeDate;
+    }
+
+    public void setCompleteDate(Date completeDate) {
+        this.completeDate = completeDate;
+    }
+
+    public Long getShowUat() {
+        return showUat;
+    }
+
+    public void setShowUat(Long showUat) {
+        this.showUat = showUat;
+    }
+}
