6d1beae76071460d197361b90a3eb4e5d00039a2
viholovko <viholovko@gmail.com>
2016-03-02 22:17:38 +0200

    Add entity JiraBoard. Created database schemas and added repository

---
+++ src/main/java/com/swansoftwaresolutions/jirareport/core/entity/JiraBoard.java
@@ -0,0 +1,78 @@
+package com.swansoftwaresolutions.jirareport.core.entity;
+
+import javax.persistence.*;
+
+/**
+ * @author Vitaliy Holovko
+ */
+@Entity
+@Table(name = "jira_boards")
+public class JiraBoard {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "board_id")
+    private Long boardId;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "type")
+    private String type;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getBoardId() {
+        return boardId;
+    }
+
+    public void setBoardId(Long boardId) {
+        this.boardId = boardId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        JiraBoard jiraBoard = (JiraBoard) o;
+
+        if (!boardId.equals(jiraBoard.boardId)) return false;
+        if (!name.equals(jiraBoard.name)) return false;
+        return type.equals(jiraBoard.type);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = boardId.hashCode();
+        result = 31 * result + name.hashCode();
+        result = 31 * result + type.hashCode();
+        return result;
+    }
+}
---
+++ src/main/java/com/swansoftwaresolutions/jirareport/core/repository/impl/JiraBoardRepositoryImpl.java
@@ -0,0 +1,71 @@
+package com.swansoftwaresolutions.jirareport.core.repository.impl;
+
+import com.swansoftwaresolutions.jirareport.core.entity.JiraBoard;
+import com.swansoftwaresolutions.jirareport.core.repository.JiraBoardRepository;
+import com.swansoftwaresolutions.jirareport.core.repository.exception.NoSuchEntityException;
+import org.hibernate.Criteria;
+import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Restrictions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+/**
+ * @author Vitaliy Holovko
+ */
+
+@Repository
+@Transactional
+public class JiraBoardRepositoryImpl implements JiraBoardRepository {
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    @Override
+    public List<JiraBoard> findAll() {
+        return sessionFactory.getCurrentSession().createCriteria(JiraBoard.class).setResultTransformer(Criteria.DISTINCT_ROOT_ENTITY).list(); }
+
+    @Override
+    public JiraBoard add(JiraBoard jiraBoard) {
+        return null;
+    }
+
+    @Override
+    public JiraBoard findById(Long jiraBoardId) {
+        return (JiraBoard) sessionFactory.openSession()
+                .createCriteria(JiraBoard.class).add(Restrictions.eq("id", jiraBoardId)).uniqueResult();
+
+    }
+
+    @Override
+    public void delete(JiraBoard jiraBoard) throws NoSuchEntityException {
+        JiraBoard deleteJiraBoard = findById(jiraBoard.getId());
+
+        if (deleteJiraBoard != null) {
+            sessionFactory.getCurrentSession().delete(jiraBoard);
+        } else {
+            throw new NoSuchEntityException("Entity Not Found");
+        }
+
+    }
+
+    @Override
+    public void delete(Long jiraBoardId) throws NoSuchEntityException {
+        JiraBoard jiraBoard = findById(jiraBoardId);
+        if (jiraBoard != null) {
+            sessionFactory.getCurrentSession().delete(jiraBoard);
+        } else {
+            throw new NoSuchEntityException("Entity Not Found");
+        }
+    }
+
+    @Override
+    public JiraBoard update(JiraBoard jiraBoard) throws NoSuchEntityException {
+        if (findById(jiraBoard.getId()) == null) {
+            throw new NoSuchEntityException("Entity not found");
+        }
+        return (JiraBoard) sessionFactory.openSession().merge(jiraBoard);
+    }
+}
