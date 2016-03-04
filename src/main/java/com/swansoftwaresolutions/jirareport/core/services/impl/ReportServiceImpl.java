6666ef051d6069f490c94112b0989d98b52fbd54
viholovko <viholovko@gmail.com>
2016-03-04 18:47:53 +0200

    Add ReportService and implementation

--- src/main/java/com/swansoftwaresolutions/jirareport/core/services/impl/ReportServiceImpl.java
+++ src/main/java/com/swansoftwaresolutions/jirareport/core/services/impl/ReportServiceImpl.java
@@ -1,8 +1,31 @@
 package com.swansoftwaresolutions.jirareport.core.services.impl;
 
+import com.swansoftwaresolutions.jirareport.core.entity.Report;
+import com.swansoftwaresolutions.jirareport.core.repository.ReportRepository;
+import com.swansoftwaresolutions.jirareport.core.repository.exception.NoSuchEntityException;
+import com.swansoftwaresolutions.jirareport.core.services.ReportService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 /**
  * @author Vitaliy Holovko
  *         on 04.03.16.
  */
-public class ReportServiceImpl {
+
+@Service
+public class ReportServiceImpl implements ReportService{
+
+    @Autowired
+    ReportRepository reportRepository;
+
+
+    @Override
+    public Report save(Report project) {
+        return reportRepository.add(project);
+    }
+
+    @Override
+    public Report update(Report report) throws NoSuchEntityException {
+        return reportRepository.update(report);
+    }
 }
