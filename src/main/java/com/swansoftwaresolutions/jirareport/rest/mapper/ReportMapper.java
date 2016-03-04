02b6a3954993ad4a2f1064e466e3d0276604c1fe
viholovko <viholovko@gmail.com>
2016-03-04 18:49:08 +0200

    Created ReportMapper

--- src/main/java/com/swansoftwaresolutions/jirareport/rest/mapper/ReportMapper.java
+++ src/main/java/com/swansoftwaresolutions/jirareport/rest/mapper/ReportMapper.java
@@ -1,8 +1,18 @@
 package com.swansoftwaresolutions.jirareport.rest.mapper;
 
+import com.swansoftwaresolutions.jirareport.core.entity.Report;
+import com.swansoftwaresolutions.jirareport.rest.dto.ReportDto;
+
+import java.util.List;
+
 /**
  * @author Vitaliy Holovko
  *         on 04.03.16.
  */
 public interface ReportMapper {
+
+    ReportDto toDto(Report report);
+    List<ReportDto> toDtos(List<Report> reportList);
+    Report fromDto(ReportDto reportDto);
+    List<Report> fromDtos(List<ReportDto> reportDto);
 }
--- src/main/java/com/swansoftwaresolutions/jirareport/rest/mapper/impl/ReportMapperImpl.java
+++ src/main/java/com/swansoftwaresolutions/jirareport/rest/mapper/impl/ReportMapperImpl.java
@@ -1,8 +1,46 @@
 package com.swansoftwaresolutions.jirareport.rest.mapper.impl;
 
+import com.swansoftwaresolutions.jirareport.core.entity.Report;
+import com.swansoftwaresolutions.jirareport.rest.dto.JiraUserDto;
+import com.swansoftwaresolutions.jirareport.rest.dto.ReportDto;
+import com.swansoftwaresolutions.jirareport.rest.mapper.ReportMapper;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.lang.reflect.Type;
+import java.util.List;
+
 /**
  * @author Vitaliy Holovko
  *         on 04.03.16.
  */
-public class ReportMapperImpl {
+@Component
+public class ReportMapperImpl implements ReportMapper {
+
+    @Autowired
+    ModelMapper modelMapper;
+
+    @Override
+    public ReportDto toDto(Report report) {
+        return modelMapper.map(report, ReportDto.class);
+    }
+
+    @Override
+    public List<ReportDto> toDtos(List<Report> reportList) {
+        Type targetistType = new TypeToken<List<ReportDto>>(){}.getType();
+        return modelMapper.map(reportList, targetistType);
+    }
+
+    @Override
+    public Report fromDto(ReportDto reportDto) {
+        return modelMapper.map(reportDto, Report.class);
+    }
+
+    @Override
+    public List<Report> fromDtos(List<ReportDto> reportDtos) {
+        Type targetistType = new TypeToken<List<Report>>(){}.getType();
+        return modelMapper.map(reportDtos, targetistType);
+    }
 }
