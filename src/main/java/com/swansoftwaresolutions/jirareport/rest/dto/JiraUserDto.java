88e025505fb0de503dd411f0fcbf55ed4b93c529
viholovko <viholovko@gmail.com>
2016-03-06 23:59:30 +0200

    Modify JiraUserDto model

--- src/main/java/com/swansoftwaresolutions/jirareport/rest/dto/JiraUserDto.java
+++ src/main/java/com/swansoftwaresolutions/jirareport/rest/dto/JiraUserDto.java
@@ -5,6 +5,22 @@ package com.swansoftwaresolutions.jirareport.rest.dto;
  *         on 04.03.16.
  */
 public class JiraUserDto {
-    public Long id;
-    public String fullName;
+    private Long id;
+    private String fullName;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
 }
