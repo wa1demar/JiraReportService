805a83b1df8355f600ecbc58e7496a584dd0560f
Vitaliy <viholovko@gmail.com>
2016-03-07 18:07:15 +0200

    Add CommentDto model

--- src/main/java/com/swansoftwaresolutions/jirareport/rest/dto/CommentDto.java
+++ src/main/java/com/swansoftwaresolutions/jirareport/rest/dto/CommentDto.java
@@ -8,9 +8,58 @@ import java.util.Date;
  */
 public class CommentDto implements Serializable {
 
-    public Long id;
-    public Long reportId;
-    public String text;
-    public String creator;
-    public Date createdDate;
+    private Long id;
+    private Long reportId;
+    private Long sprintId;
+    private String text;
+    private String creator;
+    private Date createdDate;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getReportId() {
+        return reportId;
+    }
+
+    public void setReportId(Long reportId) {
+        this.reportId = reportId;
+    }
+
+    public Long getSprintId() {
+        return sprintId;
+    }
+
+    public void setSprintId(Long sprintId) {
+        this.sprintId = sprintId;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getCreator() {
+        return creator;
+    }
+
+    public void setCreator(String creator) {
+        this.creator = creator;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
 }
