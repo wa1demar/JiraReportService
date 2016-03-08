91b6b51ac0c3084dea641e1a4a00a2ed53e94320
Vitaliy <viholovko@gmail.com>
2016-03-08 18:06:51 +0200

    Add chenges to ResponceCommentDto

--- src/main/java/com/swansoftwaresolutions/jirareport/rest/dto/responce/ResponceCommentDto.java
+++ src/main/java/com/swansoftwaresolutions/jirareport/rest/dto/responce/ResponceCommentDto.java
@@ -1,6 +1,9 @@
 package com.swansoftwaresolutions.jirareport.rest.dto.responce;
 
+import com.swansoftwaresolutions.jirareport.rest.dto.CommentDto;
+
 import java.io.Serializable;
+import java.util.List;
 
 /**
  * @author Vitaliy Holovko
@@ -8,12 +11,22 @@ import java.io.Serializable;
 
 public class ResponceCommentDto implements Serializable{
 
-    Boolean success;
-    String  message;
+    private Boolean success;
+    private String  message;
+    private CommentDto commentDto;
+    private List<CommentDto> commentDtoList;
+
 
     public ResponceCommentDto(boolean success, String message) {
         this.success = success;
         this.message = message;
+    }
+
+    public ResponceCommentDto(Boolean success, String message, CommentDto commentDto, List<CommentDto> commentDtoList) {
+        this.success = success;
+        this.message = message;
+        this.commentDto = commentDto;
+        this.commentDtoList = commentDtoList;
     }
 
     public Boolean getSuccess() {
@@ -30,5 +43,21 @@ public class ResponceCommentDto implements Serializable{
 
     public void setMessage(String message) {
         this.message = message;
+    }
+
+    public CommentDto getCommentDto() {
+        return commentDto;
+    }
+
+    public void setCommentDto(CommentDto commentDto) {
+        this.commentDto = commentDto;
+    }
+
+    public List<CommentDto> getCommentDtoList() {
+        return commentDtoList;
+    }
+
+    public void setCommentDtoList(List<CommentDto> commentDtoList) {
+        this.commentDtoList = commentDtoList;
     }
 }
