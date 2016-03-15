3732d8869ee62cf4ce44cafb74b734ae37dc035e
Sergey Levchenko <bossi4ek@gmail.com>
2016-03-15 19:05:08 +0200

    work with report element template (sprintTeam table)

--- src/main/webapp/scripts/controllers/reportElement.js
+++ src/main/webapp/scripts/controllers/reportElement.js
@@ -66,12 +66,29 @@ jiraPluginApp.controller('ReportElementCtrl',
                                 endDate : 1458252000000,
                                 completeDate : null,
                                 showUat : false,
+
+                                targetPoints: 1,
+                                targetHours: 1,
+                                targetQatDefectMin: 1,
+                                targetQatDefectMax: 1,
+                                targetQatDefectHours: 1,
+                                targetUatDefectMin: 1,
+                                targetUatDefectMax: 1,
+                                targetUatDefectHours: 1,
+                                actualPoints: 1,
+                                actualHours: 1,
+                                actualQatDefectPoints: 1,
+                                actualQatDefectHours: 1,
+                                actualUatDefectPoints: 1,
+                                actualUatDefectHours: 1,
+
                                 sprintTeam: [
                                     {
                                         devName: "bridoux",
                                         engineerLevel: 3,
                                         participationLevel: "0.5",
                                         daysInSprint: 5,
+
                                         targetPoints: 5,
                                         targetHours: 5,
                                         defectMin: 5,
@@ -79,7 +96,15 @@ jiraPluginApp.controller('ReportElementCtrl',
                                         defectHours: 5,
                                         uatDefectMin: 5,
                                         uatDefectMax: 5,
-                                        uatDefectHours: 5
+                                        uatDefectHours: 5,
+
+                                        actualPoints: 5,
+                                        defectActual: 5,
+                                        defectTargetHours: 5,
+                                        defectActualHours: 5,
+                                        uatDefectActual: 5,
+                                        uatDefectTargetHours: 5,
+                                        uatDefectActualHours: 5
                                     },
                                     {
                                         id: 2,
@@ -94,7 +119,15 @@ jiraPluginApp.controller('ReportElementCtrl',
                                         defectHours: 5,
                                         uatDefectMin: 5,
                                         uatDefectMax: 5,
-                                        uatDefectHours: 5
+                                        uatDefectHours: 5,
+
+                                        actualPoints: 5,
+                                        defectActual: 5,
+                                        defectTargetHours: 5,
+                                        defectActualHours: 5,
+                                        uatDefectActual: 5,
+                                        uatDefectTargetHours: 5,
+                                        uatDefectActualHours: 5
                                     }
                                 ]
                             },
@@ -109,7 +142,7 @@ jiraPluginApp.controller('ReportElementCtrl',
                                 startDate : 1457992800000,
                                 endDate : 1458252000000,
                                 completeDate : null,
-                                showUat : false,
+                                showUat : true,
                                 sprintTeam: [
                                     {
                                         id: 7,
@@ -124,7 +157,15 @@ jiraPluginApp.controller('ReportElementCtrl',
                                         defectHours: 5,
                                         uatDefectMin: 5,
                                         uatDefectMax: 5,
-                                        uatDefectHours: 5
+                                        uatDefectHours: 5,
+
+                                        actualPoints: 5,
+                                        defectActual: 5,
+                                        defectTargetHours: 5,
+                                        defectActualHours: 5,
+                                        uatDefectActual: 5,
+                                        uatDefectTargetHours: 5,
+                                        uatDefectActualHours: 5
                                     },
                                     {
                                         id: 8,
@@ -139,7 +180,15 @@ jiraPluginApp.controller('ReportElementCtrl',
                                         defectHours: 5,
                                         uatDefectMin: 5,
                                         uatDefectMax: 5,
-                                        uatDefectHours: 5
+                                        uatDefectHours: 5,
+
+                                        actualPoints: 5,
+                                        defectActual: 5,
+                                        defectTargetHours: 5,
+                                        defectActualHours: 5,
+                                        uatDefectActual: 5,
+                                        uatDefectTargetHours: 5,
+                                        uatDefectActualHours: 5
                                     }
                                 ]
                             }
@@ -149,173 +198,14 @@ jiraPluginApp.controller('ReportElementCtrl',
                 console.log($scope.report);
             };
 
-//----------------------------------------------------------------------------------------------------------------------
-//get sprints data
-            this.getSprints = function () {
-
-                var startDate = new Date();
-                var endDate = new Date();
-                endDate.setDate(endDate.getDate()+5);
-                //TODO get sprints
-                //$scope.sprints = SprintsFactory.query({reportId: $routeParams.reportId});
-                $scope.sprints = [
-                    {
-                        id: 1,
-                        name: 'Sprint 1',
-                        type: 1,
-                        notCountTarget: true,
-                        showUat: true,
-                        startDate: startDate,
-                        endDate: endDate,
-                        state: "active"
-                    },
-                    {
-                        id: 2,
-                        name: 'Sprint 2',
-                        type: 0,
-                        notCountTarget: true,
-                        showUat: false,
-                        startDate: startDate,
-                        endDate: endDate,
-                        state: "active"
-                    },
-                    {
-                        id: 3,
-                        name: 'Sprint 3',
-                        type: 2,
-                        notCountTarget: false,
-                        showUat: false,
-                        startDate: startDate,
-                        endDate: endDate,
-                        state: "active"
-                    }
-                ];
-            };
-
-//----------------------------------------------------------------------------------------------------------------------
-//get sprint teams data
-            this.getSprintTeams = function (data) {
-
-                if (data === undefined) {
-                    data = {id: $scope.sprints[0]};
-                }
-
-                //TODO add get sprint teams by reportId and agileSprintId
-                //SprintTeamFactory.get({
-                //    reportId: $routeParams.reportId,
-                //    sprintId: data.id
-                //}, function (data) {
-                //    console.log(data);
-                //    //$scope.sprintTeams = data;
-                //}, function (error) {
-                //    console.log(error);
-                //    $scope.sprintTeams = [];
-                //});
-
-
-                if (data.id === 57283) {
-                    $scope.sprintTeams = [
-                        {
-                            id: 1,
-                            devName: "bridoux",
-                            engineerLvl: 3,
-                            participationLvl: "0.5",
-                            daysInSprint: 5
-                        },
-                        {
-                            id: 2,
-                            devName: "bmurga",
-                            engineerLvl: 1,
-                            participationLvl: "0.6",
-                            daysInSprint: 7
-                        }
-                    ];
-                } else {
-                    $scope.sprintTeams = [];
-                }
-            };
-
             self.getReportWithSprintsAndTeamsData();
 
             $scope.getReportAllData = function (item) {
                 console.log(item);
             };
 
-            $scope.showSprintDetailsData = false;
             $scope.showSprintDetails = function (item) {
-                //$scope.showSprintDetailsData = !$scope.showSprintDetailsData;
 
-                //$(".sprint-id[data-id-sprint='"+item.id+"']").parent().after("test");
-
-                $scope.showSprintTeam(item);
-
-                //console.log(item);
-            };
-
-            $scope.showSprintTeam = function (data) {
-                var object =  $("td[data-id-sprint='"+data.id+"']");
-                var idSprint = data.id;
-                var state = data.state;
-                var idSprintForChart = idSprint;
-
-                $(".sprint-team-tr[data-id-sprint!='"+idSprint+"']").hide();
-                $(".sprint-id[data-id!='"+idSprint+"']").removeAttr("rowspan");
-                $(".sprint-id[data-id!='"+idSprint+"']").removeClass("open");
-
-                if ($(".sprint-team-tr[data-id-sprint='"+idSprint+"']").eq(0).is(":visible")) {
-                    $(".sprint-team-tr[data-id-sprint='"+idSprint+"']").hide();
-                    $(object).parent().find(".sprint-id").removeAttr("rowspan");
-                    $(object).parent().find(".sprint-id").removeClass("open");
-
-                    idSprintForChart = 0;
-                } else {
-                    if (!$(".sprint-team-tr[data-id-sprint='"+idSprint+"']").length) {
-                        $scope.getSprintTeam(idSprint, state);
-                    }
-                    $(".sprint-team-tr[data-id-sprint='"+idSprint+"']").show();
-                    $(object).parent().find(".sprint-id").attr("rowspan", $(".sprint-team-tr[data-id-sprint='"+idSprint+"']").length+1);
-                    $(object).parent().find(".sprint-id").addClass("open");
-                }
-
-                ////Change chart title
-                //var chartTitle = idSprintForChart == 0 ? "Project to date" : $(".show-sprint-details[data-id-sprint='"+idSprintForChart+"']").data("name");
-                //$("#chart-title").text(chartTitle);
-                //
-                ////Show chart
-                //createChartNew(idSprintForChart);
-                //
-                ////Show Progress bar
-                //changeProgressBar(idSprintForChart);
-            };
-
-            $scope.getSprintTeam = function(idSprint, state) {
-
-                $(".sprint-id[data-id-sprint='"+idSprint+"']").parent().after('<tr ng-repeat="dataSprint in data.sprintTeam" class="sprint-team-tr" data-id-sprint="{{dataSprint.id}}"><td>xxx</td></tr>');
-                $(".sprint-id[data-id-sprint='"+idSprint+"']").attr("rowspan", $(".sprint-team-tr[data-id-sprint='"+idSprint+"']").length+1);
-
-                //$(".sprint-team[data-id='"+idSprint+"'] .td-for-sprint-team").html('<span class="aui-icon aui-icon-wait">Loading...</span>');
-                //return false;
-
-
-                //AJS.$.ajax({
-                //    url: config.pathToAjax + "/plugins/servlet/getreportdata",
-                //    type: "POST",
-                //    data: ({
-                //        action:     "getSprintTeam",
-                //        idReport:   $("#change-report-element option:selected").val(),
-                //        idBoard:    $("#change-report-element option:selected").data("boardId"),
-                //        idSprint:   idSprint,
-                //        state:      state
-                //    }),
-                //    //dataType: "json",
-                //    beforeSend: function () {
-                //
-                //    },
-                //    success: function(response){
-                //        $(".sprint-id[data-id-sprint='"+idSprint+"']").parent().after(response);
-                //        $(".sprint-id[data-id-sprint='"+idSprint+"']").attr("rowspan", $(".sprint-team-tr[data-id-sprint='"+idSprint+"']").length+1);
-                //    }
-                //});
             };
 
         }
