de29a862ae618ba0bc44dd0a3c4da4281a6c4795
Sergey Levchenk <bossi4ek@gmail.com>
2016-03-16 02:36:37 +0200

    fix indicators directive

--- src/main/webapp/scripts/directives/directives.js
+++ src/main/webapp/scripts/directives/directives.js
@@ -1,53 +1,63 @@
 'use strict';
 
 jiraPluginApp.directive('printThis', function() {
-  /** https://docs.angularjs.org/guide/directive **/
+    /** https://docs.angularjs.org/guide/directive **/
 
-  var directiveDefinitionObject = {
-    restrict: 'EA',
-    template: '<input ng-model="data" placeholder="Type here"/> {{ data }}'
-  }
+    var directiveDefinitionObject = {
+        restrict: 'EA',
+        template: '<input ng-model="data" placeholder="Type here"/> {{ data }}'
+    }
 
-  return directiveDefinitionObject;
+    return directiveDefinitionObject;
 });
 
 //directive for indicators
 jiraPluginApp.directive('indicator', function() {
-  var directive = {
-      restrict: 'E',
-      template: "<b>{{show}}</b> <br/>"
-  };
-
-  directive.scope = {
-    show:     "=show",
-    target :  "=target",
-    actual :  "=actual"
-  };
-
-  directive.compile = function(element, attributes) {
-    element.css("border", "1px solid #cccccc");
-
-    var linkFunction = function($scope, element, attributes) {
-      element.html("<b>"+$scope.show +"</b> <br/>");
-      element.css("background-color", "#ff00ff");
+    var directive = {
+        restrict: 'E',
+        template: "<b>{{show}}</b> <br/>"
     };
 
-    return linkFunction;
-  };
+    directive.scope = {
+        show:     "=show",
+        target :  "=target",
+        actual :  "=actual"
+    };
 
-  return directive;
+    directive.compile = function(element, attributes) {
+        //element.css("border", "1px solid #cccccc");
+
+        var linkFunction = function($scope, element, attributes) {
+
+            var attrClass = "";
+            if ($scope.actual >= $scope.target) {
+                attrClass = "circle green";
+            } else if ($scope.actual < $scope.target && ((($scope.target - $scope.actual)) * 100) / $scope.target <= 10) {
+                attrClass = "circle yellow";
+            } else {
+                attrClass = "circle red";
+            }
+
+            element.html('<span class="'+attrClass+'"></span>');
+            //element.css("background-color", "#ff00ff");
+        };
+
+        return linkFunction;
+    };
+
+    return directive;
 });
 
 jiraPluginApp.directive('convertToNumber', function() {
-  return {
-    require: 'ngModel',
-    link: function(scope, element, attrs, ngModel) {
-      ngModel.$parsers.push(function(val) {
-        return parseInt(val, 10);
-      });
-      ngModel.$formatters.push(function(val) {
-        return '' + val;
-      });
-    }
-  };
+    return {
+        require: 'ngModel',
+        link: function(scope, element, attrs, ngModel) {
+            ngModel.$parsers.push(function(val) {
+                return parseInt(val, 10);
+            });
+            ngModel.$formatters.push(function(val) {
+                return '' + val;
+            });
+        }
+    };
 });
