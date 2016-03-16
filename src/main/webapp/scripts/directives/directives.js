c8d88af54c1373b0e79a086911bfaa024c390782
Sergey Levchenk <bossi4ek@gmail.com>
2016-03-16 08:22:30 +0200

    update indicators directive

--- src/main/webapp/scripts/directives/directives.js
+++ src/main/webapp/scripts/directives/directives.js
@@ -20,22 +20,48 @@ jiraPluginApp.directive('indicator', function() {
 
     directive.scope = {
         show:     "=show",
+        type:     "=type",
         target :  "=target",
-        actual :  "=actual"
+        actual :  "=actual",
+        min :     "=min",
+        max :     "=max"
     };
 
     directive.compile = function(element, attributes) {
         //element.css("border", "1px solid #cccccc");
 
         var linkFunction = function($scope, element, attributes) {
-
             var attrClass = "";
-            if ($scope.actual >= $scope.target) {
-                attrClass = "circle green";
-            } else if ($scope.actual < $scope.target && ((($scope.target - $scope.actual)) * 100) / $scope.target <= 10) {
-                attrClass = "circle yellow";
-            } else {
-                attrClass = "circle red";
+            if ($scope.type === "iconsVelocity") {
+                if ($scope.actual >= $scope.target) {
+                    attrClass = "circle green";
+                } else if ($scope.actual < $scope.target && ((($scope.target - $scope.actual)) * 100) / $scope.target <= 10) {
+                    attrClass = "circle yellow";
+                } else {
+                    attrClass = "circle red";
+                }
+            } else if ($scope.type === "icons") {
+                if ($scope.actual  == 0 && $scope.min == 0 && $scope.max == 0) {
+                    attrClass = "star-five";
+                } else if ($scope.actual < $scope.min) {
+                    attrClass = "star-five";
+                } else if ($scope.actual >= $scope.min && $scope.actual <= $scope.max) {
+                    attrClass = "circle green";
+                } else if ($scope.actual > $scope.max && ((($scope.actual - $scope.max)) * 100) / $scope.max <= 10) {
+                    attrClass = "circle yellow";
+                } else {
+                    attrClass = "circle red";
+                }
+            } else if ($scope.type === "iconsHours") {
+                if ($scope.actual == 0) {
+                    attrClass = "star-five";
+                } else if ($scope.actual <= $scope.target) {
+                    attrClass = "circle green";
+                } else if ($scope.actual > $scope.target && ((($scope.actual - $scope.target)) * 100) / $scope.target <= 10) {
+                    attrClass = "circle yellow";
+                } else {
+                    attrClass = "circle red";
+                }
             }
 
             element.html('<span class="'+attrClass+'"></span>');
