61a6ce2b454f302c4cae233d1a91b7a25532e5e2
Vitaliy <viholovko@gmail.com>
2016-03-17 18:50:22 +0200

    Changes in IssueRestClient

--- src/main/java/com/swansoftwaresolutions/jirareport/sheduller/rest/client/IssueRestClient.java
+++ src/main/java/com/swansoftwaresolutions/jirareport/sheduller/rest/client/IssueRestClient.java
@@ -1,7 +1,14 @@
 package com.swansoftwaresolutions.jirareport.sheduller.rest.client;
 
+import com.swansoftwaresolutions.jirareport.core.dto.PointDto;
+import com.swansoftwaresolutions.jirareport.core.dto.JiraUserDto;
+import com.swansoftwaresolutions.jirareport.core.dto.report.ReportDto;
 import com.swansoftwaresolutions.jirareport.core.service.JiraBoardService;
+import com.swansoftwaresolutions.jirareport.core.service.JiraUserService;
+import com.swansoftwaresolutions.jirareport.core.service.PointService;
+import com.swansoftwaresolutions.jirareport.core.service.ReportService;
 import com.swansoftwaresolutions.jirareport.domain.entity.JiraBoard;
+import com.swansoftwaresolutions.jirareport.domain.repository.exception.NoSuchEntityException;
 import com.swansoftwaresolutions.jirareport.sheduller.dto.*;
 import com.swansoftwaresolutions.jirareport.sheduller.job.RestClient;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +17,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.List;
+import java.util.*;
 import java.util.logging.Logger;
 
 /**
@@ -30,24 +37,132 @@ public class IssueRestClient extends RestClientBase implements RestClient {
     @Autowired
     JiraBoardService jiraBoardService;
 
+    @Autowired
+    JiraUserService jiraUserService;
+
+    @Autowired
+    PointService pointService;
+
+    @Autowired
+    ReportService reportService;
+
     @Override
     public void loadData() {
         List<JiraBoard> jiraBoardList = jiraBoardService.findAll();
 
-        for (JiraBoard jiraBoard: jiraBoardList) {
+        combineInformationForPoint(jiraBoardList);
+    }
+
+    private void combineInformationForPoint(List<JiraBoard> jiraBoardList) {
+        for (JiraBoard jiraBoard : jiraBoardList) {
             HttpEntity<String> request = new HttpEntity<>(getHeaders());
             RestTemplate restTemplate = new RestTemplate();
-            AgileSprintsDto agileSprints = restTemplate.exchange(URL_SPRINT.replace("{boardId}", String.valueOf(jiraBoardList.get(1).getBoardId())), HttpMethod.GET, request, AgileSprintsDto.class).getBody();
+            AgileSprintsDto agileSprints = restTemplate.exchange(URL_SPRINT.replace("{boardId}", String.valueOf(jiraBoard.getBoardId())), HttpMethod.GET, request, AgileSprintsDto.class).getBody();
 
-            for (AgileSprintDto agileSprintDto : agileSprints.values) {
-                IssuesDto issues = restTemplate.exchange(URL_ISSUES_BY_SPRINT.replace("{sprintId}", String.valueOf(agileSprintDto.id)), HttpMethod.GET, request, IssuesDto.class).getBody();
-                for (IssueDto issueDto : issues.issues) {
-                    convertIssueDtoToJiraIssueDto(issueDto);
+            try {
+                getAllIsuues(agileSprints);
+            } catch (NoSuchEntityException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void getAllIsuues(AgileSprintsDto agileSprints) throws NoSuchEntityException {
+        for (AgileSprintDto agileSprintDto : agileSprints.values) {
+            HttpEntity<String> request = new HttpEntity<>(getHeaders());
+            RestTemplate restTemplate = new RestTemplate();
+
+            IssuesDto issues = restTemplate.exchange(URL_ISSUES_BY_SPRINT.replace("{sprintId}", String.valueOf(agileSprintDto.id)), HttpMethod.GET, request, IssuesDto.class).getBody();
+            List<JiraIssueDto> jiraIssueList = new ArrayList<>();
+            for (IssueDto issueDto : issues.issues) {
+                jiraIssueList.add(convertIssueDtoToJiraIssueDto(issueDto));
+            }
+
+//            ReportDto report = new ReportDto();
+//            try {
+//                report = reportService.findByBoardId(new Long(agileSprintDto.originBoardId));
+//            } catch (NoSuchEntityException ex){
+//                log.warning("NoSuchEntityException Report");
+//            }
+
+            Map<String, List<JiraIssueDto>> mapPoints = grupByUser(jiraIssueList);
+
+            List<PointDto> points = new ArrayList<>();
+
+            for (Map.Entry<String, List<JiraIssueDto>> entry : mapPoints.entrySet()) {
+                String key = entry.getKey();
+                List<JiraIssueDto> thing = entry.getValue();
+
+                PointDto pointDto = new PointDto();
+                pointDto.setUserLogin(key);
+                pointDto.setSprintId(agileSprintDto.id);
+                try {
+
+                    for (JiraIssueDto jiraIssueDto : thing) {
+                        if (!jiraIssueDto.getIssueTypeName().equals("Bug")) {
+                            pointDto.setIssueCount(pointDto.getIssueCount()+1);
+                            pointDto.setIssueHourse(checkNotNull(pointDto.getIssueHourse())+checkNotNull(jiraIssueDto.getTimeSpent()));
+                            pointDto.setPoints(pointDto.getPoints() +jiraIssueDto.getPoints());
+                        } else {
+//                            if (report!=null && checkAdmin(report.getAdmins(), jiraIssueDto.getCreatorName())){
+//                                pointDto.setBugUATCount(pointDto.getBugQATCount()+1);
+//                                pointDto.setBugUATHours(checkNotNull(pointDto.getBugUATHours()) + checkNotNull(jiraIssueDto.getTimeSpent()));
+//                            } else {
+                                pointDto.setBugQATCount(pointDto.getBugQATCount() + 1);
+                                pointDto.setBugQATHourse(checkNotNull(pointDto.getBugQATHourse())+checkNotNull(jiraIssueDto.getTimeSpent()));
+//                            }
+
+                        }
+                    }
+
+                } catch (NullPointerException ec) {
+                    log.warning("!!!!!!!!!!");
+                } catch (Exception sj) {
+                    log.warning("!!!!!!!!!!");
                 }
+                points.add(pointDto);
+                pointDto = pointService.add(pointDto);
+            }
 
+            log.info("completed " + points.size());
+
+
+        }
+
+    }
+
+    private boolean checkAdmin(List<JiraUserDto> admins, String creatorName) {
+        for(JiraUserDto jiraIssueDto: admins){
+            if (jiraIssueDto.getLogin().equals(creatorName)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private Long checkNotNull(Long timeSpent) {
+        if (timeSpent == null) {
+            return 0L;
+        }
+        return timeSpent;
+    }
+
+    private Map<String, List<JiraIssueDto>> grupByUser(List<JiraIssueDto> jiraIssueList) {
+        Map<String, List<JiraIssueDto>> mapPoints = new HashMap<>();
+        for (JiraIssueDto jiraIssue : jiraIssueList) {
+            if (mapPoints.containsKey(jiraIssue.getAssignetName())) {
+                List<JiraIssueDto> list = mapPoints.get(jiraIssue.getAssignetName());
+                list.add(jiraIssue);
+                mapPoints.put(jiraIssue.getAssignetName(), list);
+            } else {
+                List<JiraIssueDto> list = new ArrayList<>();
+                list.add(jiraIssue);
+
+                mapPoints.put(jiraIssue.getAssignetName(), list);
             }
         }
 
+        return mapPoints;
     }
 
     private JiraIssueDto convertIssueDtoToJiraIssueDto(IssueDto issueDto) {
