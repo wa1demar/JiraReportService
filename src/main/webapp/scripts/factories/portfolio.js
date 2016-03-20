7f453c9b2a1fa0ccdbcbef6325b8a6fd1b5f076c
Sergey Levchenk <bossi4ek@gmail.com>
2016-03-21 00:32:54 +0200

    add api url for portfolio page

--- src/main/webapp/scripts/factories/portfolio.js
+++ src/main/webapp/scripts/factories/portfolio.js
@@ -2,6 +2,6 @@
 
 jiraPluginApp.factory('PortfoliosFactory', ['$resource', 'CONFIG', function ($resource, CONFIG) {
   return $resource(CONFIG.API_PATH + '/portfolios', {}, {
-    query:  { method: 'GET', isArray: false }
+    query:  { method: 'GET', isArray: true }
   });
 }]);
