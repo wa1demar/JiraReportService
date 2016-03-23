d349e2aa6e74484768ac3024fb509aa7600d6d0f
waldemar <zehelloworld@gmail.com>
2016-03-23 13:03:42 +0200

    update profile

--- src/main/java/com/swansoftwaresolutions/jirareport/core/service/impl/UserServiceImpl.java
+++ src/main/java/com/swansoftwaresolutions/jirareport/core/service/impl/UserServiceImpl.java
@@ -1,11 +1,15 @@
 package com.swansoftwaresolutions.jirareport.core.service.impl;
 
 import com.swansoftwaresolutions.jirareport.core.mapper.UserMapper;
+import com.swansoftwaresolutions.jirareport.domain.entity.User;
 import com.swansoftwaresolutions.jirareport.domain.repository.UserRepository;
 import com.swansoftwaresolutions.jirareport.core.service.UserService;
 import com.swansoftwaresolutions.jirareport.core.dto.UserDto;
 import com.swansoftwaresolutions.jirareport.core.dto.UserLoginDto;
+import com.swansoftwaresolutions.jirareport.domain.repository.exception.NoSuchEntityException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 /**
@@ -31,6 +35,20 @@ public class UserServiceImpl implements UserService {
         UserLoginDto userLoginDto = userMapper.loginToDto(userRepository.findByUsername(username));
         userLoginDto.setToken("xfdsfdgdfgfhfghfg ");
         return userLoginDto;
+    }
+
+    @Override
+    public UserDto update(UserDto userDto) throws NoSuchEntityException {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        User user = userRepository.findByUsername(auth.getName());
+
+        user.setEmail(userDto.getEmail());
+        user.setFullName(userDto.getFullName());
+        user.setUsername(userDto.getUsername());
+
+        User updatedUser = userRepository.update(user);
+
+        return userMapper.toDto(updatedUser);
     }
 
 
--- src/main/java/com/swansoftwaresolutions/jirareport/web/controller/UserController.java
+++ src/main/java/com/swansoftwaresolutions/jirareport/web/controller/UserController.java
@@ -2,14 +2,20 @@ package com.swansoftwaresolutions.jirareport.web.controller;
 
 import com.swansoftwaresolutions.jirareport.core.dto.UserDto;
 import com.swansoftwaresolutions.jirareport.core.service.UserService;
+import com.swansoftwaresolutions.jirareport.domain.repository.exception.NoSuchEntityException;
+import com.swansoftwaresolutions.jirareport.web.exception.InvalidRequestException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 /**
  * @author Vladimir Martynyuk
@@ -26,8 +32,20 @@ public class UserController {
 
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         UserDto userDto = userService.retrieveByUsername(auth.getName());
-        userDto.setPassword(null);
 
         return new ResponseEntity<>(userDto, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/profile", method = RequestMethod.PUT)
+    private ResponseEntity<UserDto> updateProfile(@Valid @RequestBody UserDto userDto, BindingResult bindingResult) throws NoSuchEntityException {
+
+        if (bindingResult.hasErrors()) {
+
+            throw new InvalidRequestException("Invalid request", bindingResult);
+        }
+
+        UserDto updatedUserDto = userService.update(userDto);
+
+        return new ResponseEntity<>(updatedUserDto, HttpStatus.OK);
     }
 }
