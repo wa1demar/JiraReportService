737300c114db40b6bfc02e6413e7dfa6729474e3
waldemar <zehelloworld@gmail.com>
2016-03-30 14:45:08 +0300

    fix tests

--- src/main/java/com/swansoftwaresolutions/jirareport/rest/client/AbstractRestClient.java
+++ src/main/java/com/swansoftwaresolutions/jirareport/rest/client/AbstractRestClient.java
@@ -1,7 +1,5 @@
 package com.swansoftwaresolutions.jirareport.rest.client;
 
-import com.sun.xml.internal.ws.developer.Serialization;
-import com.swansoftwaresolutions.jirareport.core.Settings;
 import com.swansoftwaresolutions.jirareport.core.dto.config.ConfigDto;
 import com.swansoftwaresolutions.jirareport.core.service.ConfigService;
 import org.apache.commons.codec.binary.Base64;
