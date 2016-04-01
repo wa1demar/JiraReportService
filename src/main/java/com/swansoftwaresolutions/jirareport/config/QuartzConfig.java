5e3abe220d6e4d0d3ae3bac4f569b28b5aada8e7
waldemar <zehelloworld@gmail.com>
2016-04-01 19:14:36 +0300

    scheduler

--- src/main/java/com/swansoftwaresolutions/jirareport/config/QuartzConfig.java
+++ src/main/java/com/swansoftwaresolutions/jirareport/config/QuartzConfig.java
@@ -38,8 +38,8 @@ public class QuartzConfig {
         Trigger[] triggers = {
 //                loadProjectsTrigger().getObject(),
 //                loadJiraUsersTrigger().getObject(),
-//                loadJiraBoardsTrigger().getObject(),
-//                loadIssuesTrigger().getObject()
+                loadJiraBoardsTrigger().getObject(),
+                loadIssuesTrigger().getObject()
         };
 
         quartzScheduler.setTriggers(triggers);
@@ -67,7 +67,7 @@ public class QuartzConfig {
     CronTriggerFactoryBean loadIssuesTrigger() {
         CronTriggerFactoryBean cronTriggerFactoryBean = new CronTriggerFactoryBean();
         cronTriggerFactoryBean.setJobDetail(loadIssuesJobDetail().getObject());
-        cronTriggerFactoryBean.setCronExpression("0 0/5 * * * ?");
+        cronTriggerFactoryBean.setCronExpression("0 0 12 * * ?");
 //        cronTriggerFactoryBean.setCronExpression("12 0 * * * ?");
         return cronTriggerFactoryBean;
     }
@@ -100,7 +100,7 @@ public class QuartzConfig {
     CronTriggerFactoryBean loadJiraBoardsTrigger() {
         CronTriggerFactoryBean cronTriggerFactoryBean = new CronTriggerFactoryBean();
         cronTriggerFactoryBean.setJobDetail(loadJiraBoardsJobDetail().getObject());
-        cronTriggerFactoryBean.setCronExpression("0 0/5 * * * ?");
+        cronTriggerFactoryBean.setCronExpression("0 15 0 ? * SUN");
         return cronTriggerFactoryBean;
     }
 
