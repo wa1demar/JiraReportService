3e2381b06a03728601d16ff0ef35429bd652e5c5
Sergey Levchenk <bossi4ek@gmail.com>
2016-04-07 11:50:46 +0300

    add api for due date issues

--- src/main/webapp/scripts/controllers/dueDateIssue.js
+++ src/main/webapp/scripts/controllers/dueDateIssue.js
@@ -21,98 +21,45 @@ jiraPluginApp.controller('DueDateIssueCtrl',
 
 //----------------------------------------------------------------------------------------------------------------------
 //TODO For pagination
-//            $scope.dataDueDate = [];
-//            $scope.totalDueDate = 0;
-//            $scope.dueDatePerPage = 10; // this should match however many results your API puts on one page
-//            getResultsPage(1);
-//
-//            $scope.pagination = {
-//                current: 1
-//            };
-//
-//            $scope.pageChanged = function(newPage) {
-//                getResultsPage(newPage);
-//            };
-//
-//            function getResultsPage(pageNumber) {
-//                // this is just an example, in reality this stuff should be in a service
-//                $scope.loaderShow = true;
-//                DueDateIssueFactory.query({page: pageNumber}, function(result){
-//                    $scope.dataDueDate = result.dueDate;
-//                    $scope.totalDueDate = result.totalItems;
-//                    $scope.dueDatePerPage = result.itemsPerPage;
-//                    $scope.loaderShow = false;
-//                    //$scope.setLoading(false);
-//                }, function (error) {
-//                    Notification.error("Server error");
-//                });
-//            }
+            $scope.dataDueDate = [];
+            $scope.totalDueDate = 0;
+            $scope.dueDatePerPage = 10; // this should match however many results your API puts on one page
+            getResultsPage(1);
 
-
-            this.getSystemUsers = function () {
-                //$scope.dataDueDate = [];
-                //DueDateIssueFactory.query({}, function (data) {
-                //    $scope.dataDueDate = data.dueDate;
-                //}, function (error) {
-                //    Notification.error("Server error");
-                //});
-
-                $scope.dataDueDate = [
-                    {
-                        id: 1,
-                        project: "test 1",
-                        assignee: "assignee 1",
-                        key: "key-1",
-                        summary: "summary 1",
-                        description: ["description 1", "description 2", "description 3"],
-                        status: "In Progress",
-                        dueDate: [new Date("2016-04-06"), new Date(new Date - 86400 * 1000), new Date(new Date - 172800 * 1000)]
-                    },
-                    {
-                        id: 2,
-                        project: "test 2",
-                        assignee: "assignee 2",
-                        key: "key-2",
-                        summary: "summary 2",
-                        description: ["description 2"],
-                        status: "To Do",
-                        dueDate: [new Date(new Date - 86400 * 1000)]
-                    },
-                    {
-                        id: 3,
-                        project: "test 2",
-                        assignee: "assignee 2",
-                        key: "key-2",
-                        summary: "summary 2",
-                        description: ["description 2"],
-                        status: "To Do",
-                        dueDate: [new Date("2016-04-05")]
-                    },
-                    {
-                        id: 4,
-                        project: "test 2",
-                        assignee: "assignee 2",
-                        key: "key-2",
-                        summary: "summary 2",
-                        description: ["description 2"],
-                        status: "To Do",
-                        dueDate: [new Date("2016-04-03"), new Date("2016-04-01")]
-                    },
-                    {
-                        id: 5,
-                        project: "test 2",
-                        assignee: "assignee 2",
-                        key: "key-2",
-                        summary: "summary 2",
-                        description: ["description 2"],
-                        status: "To Do",
-                        dueDate: [new Date()]
-                    }
-                ];
-
-                $scope.loaderShow = false;
+            $scope.pagination = {
+                current: 1
             };
 
-            self.getSystemUsers();
+            $scope.pageChanged = function(newPage) {
+                getResultsPage(newPage);
+            };
+
+            function getResultsPage(pageNumber) {
+                // this is just an example, in reality this stuff should be in a service
+                $scope.loaderShow = true;
+                DueDateIssueFactory.query({page: pageNumber}, function(result){
+                    $scope.dataDueDate = result.dates;
+                    $scope.totalDueDate = result.totalItems;
+                    $scope.dueDatePerPage = result.itemsPerPage;
+                    $scope.loaderShow = false;
+                    //$scope.setLoading(false);
+                }, function (error) {
+                    Notification.error("Server error");
+                });
+            }
+
+
+            //this.getSystemUsers = function () {
+            //    $scope.dataDueDate = [];
+            //    DueDateIssueFactory.query({}, function (data) {
+            //        $scope.dataDueDate = data.dates;
+            //        $scope.loaderShow = false;
+            //    }, function (error) {
+            //        Notification.error("Server error");
+            //    });
+            //
+            //};
+            //
+            //self.getSystemUsers();
         }
 ]);
--- src/main/webapp/scripts/factories/dueDateIssue.js
+++ src/main/webapp/scripts/factories/dueDateIssue.js
@@ -1,7 +1,7 @@
 'use strict';
 
 jiraPluginApp.factory('DueDateIssueFactory', ['$resource', 'CONFIG', function ($resource, CONFIG) {
-    return $resource(CONFIG.API_PATH + '/due_date_issue/:page', {page: "@page"}, {
+    return $resource(CONFIG.API_PATH + '/issues/due_dates/:page', {page: "@page"}, {
         query:          { method: 'GET' },
         get:            { method: 'GET', isArray: false }
     });
