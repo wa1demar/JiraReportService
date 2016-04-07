75871c5aabba8f2e55d65c3a4057cf50d23569a8
Vladimir <waldemar@localhost.localdomain>
2016-04-07 20:01:56 +0300

    add log for tasks

--- src/main/java/com/swansoftwaresolutions/jirareport/core/service/impl/TaskServiceImpl.java
+++ src/main/java/com/swansoftwaresolutions/jirareport/core/service/impl/TaskServiceImpl.java
@@ -12,9 +12,11 @@ import com.swansoftwaresolutions.jirareport.rest.service.GroupImporterService;
 import com.swansoftwaresolutions.jirareport.rest.service.IssueImporterService;
 import com.swansoftwaresolutions.jirareport.rest.service.ProjectImporterService;
 import com.swansoftwaresolutions.jirareport.rest.service.UserImporterService;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -22,6 +24,8 @@ import java.util.List;
  */
 @Service
 public class TaskServiceImpl implements TaskService {
+
+    final static Logger logger = Logger.getLogger(TaskServiceImpl.class);
 
     private static final String GROUPS_TASK = "groups";
     private static final String USERS_TASK = "users";
@@ -115,42 +119,56 @@ public class TaskServiceImpl implements TaskService {
     }
 
     private void startImportGroups() {
+        logger.info("Start task for groups at " + new Date());
         taskRepository.setStarted(GROUPS_TASK);
         groupImporterService.importGroupsFromJira();
         taskRepository.setStopped(GROUPS_TASK);
+        logger.info("Stop task for groups at " + new Date());
     }
 
     private void startImportUsers() {
+        logger.info("Start task for users at " + new Date());
         taskRepository.setStarted(USERS_TASK);
         userImporterService.importUsersFromJira();
         taskRepository.setStopped(USERS_TASK);
+        logger.info("Stop task for users at " + new Date());
     }
 
     private void startImportProjects() {
+        logger.info("Start task for projects at " + new Date());
         taskRepository.setStarted(PROJECTS_TASK);
         projectImporterService.importProjectsFromJira();
         taskRepository.setStopped(PROJECTS_TASK);
+        logger.info("Stop task for projects at " + new Date());
     }
 
     private void startImportBoards() {
+        logger.info("Start task for boards at " + new Date());
         taskRepository.setStarted(BOARDS_TASK);
         boardImporterService.importBoardsFromJira();
         taskRepository.setStopped(BOARDS_TASK);
+        logger.info("Stop task for boards at " + new Date());
     }
 
     private void startImportSprints() {
+        logger.info("Start task for sprints at " + new Date());
         taskRepository.setStarted(SPRINTS_TASK);
         sprintImporterService.loadSprintsFromJiraByBoard();
         taskRepository.setStopped(SPRINTS_TASK);
+        logger.info("Stop task for sprints at " + new Date());
     }
 
     private void startImportIssues() {
+        logger.info("Start task for issues at " + new Date());
         taskRepository.setStarted(ISSUES_TASK);
         issueImporterService.importIssueFromJira();
         taskRepository.setStopped(ISSUES_TASK);
+        logger.info("Stop task for issues at " + new Date());
     }
 
     private void startImportDueDate() {
+        logger.info("Start task for dueDates at " + new Date());
         dueDateImporterService.importDueDate();
+        logger.info("Stop task for dueDates at " + new Date());
     }
 }
