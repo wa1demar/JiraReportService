df135ad2b117993bbf81ee714fd9b74b7b1e4c6d
Vladimir <waldemar@localhost.localdomain>
2016-04-14 18:03:02 +0300

    add resource-colkumns table

---
+++ src/main/java/com/swansoftwaresolutions/jirareport/domain/entity/ResourceColumn.java
@@ -0,0 +1,53 @@
+package com.swansoftwaresolutions.jirareport.domain.entity;
+
+import javax.persistence.*;
+
+/**
+ * @author Vladimir Martynyuk
+ */
+@Entity
+@Table(name = "resource_columns")
+public class ResourceColumn {
+
+    private Long id;
+    private String name;
+    private String color;
+    private boolean fixed;
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Column(name = "name")
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Column(name = "color")
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    @Column(name = "fixed")
+    public boolean isFixed() {
+        return fixed;
+    }
+
+    public void setFixed(boolean fixed) {
+        this.fixed = fixed;
+    }
+}
