558cb99e432579cb3c298c47402c9b9d05e60d07
waldemar <zehelloworld@gmail.com>
2016-07-12 13:48:18 +0300

    my timescheet

--- src/main/java/com/swansoftwaresolutions/jirareport/core/service/impl/TimeSheetServiceImpl.java
+++ src/main/java/com/swansoftwaresolutions/jirareport/core/service/impl/TimeSheetServiceImpl.java
@@ -1,5 +1,6 @@
 package com.swansoftwaresolutions.jirareport.core.service.impl;
 
+import com.swansoftwaresolutions.jirareport.core.dto.LogWork;
 import com.swansoftwaresolutions.jirareport.core.dto.TimeSheetDto;
 import com.swansoftwaresolutions.jirareport.core.service.TimeSheetService;
 import com.swansoftwaresolutions.jirareport.domain.entity.JiraIssue;
@@ -11,6 +12,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -34,7 +36,22 @@ public class TimeSheetServiceImpl implements TimeSheetService {
 
         List<JiraIssue> issues = jiraIssueRepository.findByUserLogin(user.getUsername());
 
+        TimeSheetDto timeSheetDto = new TimeSheetDto();
+        timeSheetDto.setUserName(user.getFullName());
 
-        return null;
+        List<LogWork> logWorks = new ArrayList<>();
+        for (JiraIssue issue : issues) {
+            LogWork logWork = new LogWork();
+            logWork.setIssueKey(issue.getKey());
+            logWork.setDate(issue.getUpdated());
+            logWork.setDescription(issue.getDescription());
+
+            logWorks.add(logWork);
+        }
+
+        timeSheetDto.setIssues(logWorks);
+
+
+        return timeSheetDto;
     }
 }
